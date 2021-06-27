7da0727513221d640fdccdfc724e6a3042f99c58
Elena Danchenko <danchenkohelen@gmail.com>
2021-06-27 17:09:31 +0300

    if else switch statements

---
+++ CSharpTranining/0_FirstSteps/BasicofCSharp.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace CSharpTranining._0_FirstSteps
+{
+   public  class BasicofCSharp
+    {
+        public static void BasicCsharpRun()
+        {
+            //Variable declaration
+            string value;
+
+            //Assign Variable
+            value = " Have a nice day!";
+
+            //Declare and assign
+            String value2 = " Have a Great day!";
+
+            //Print Variable
+            Test.HelloName("Elena," + value);
+
+            //Assign new value for Variable
+            value = "Starting Calculation";
+
+            //Print new value
+            Console.WriteLine(value);
+
+            //Calculate a sum from our new method
+            Console.WriteLine(Sum(2, 3));
+        }
+
+        public static int Sum(int value1, int value2)
+        {
+            return value1 + value2;
+        }
+    }
+           
+}
+
--- CSharpTranining/Program.cs
+++ CSharpTranining/Program.cs
@@ -1,33 +1,94 @@
-﻿using System;
+﻿using CSharpTranining._0_FirstSteps;
+using System;
 
 namespace CSharpTranining
 {
-    class Program
+    public class Program
     {
+        private const string Question1 = "Hello! What is your name?";
+        private const string Question2 = "How old are you?";
+        private const string Question3 = "What day of the week?";
+        private const string Question4 = " What is your budget?";
+        private const string Greeting = "Hello ";
+        private static string Answer1;
+        private static string Answer2;
+        private static string Answer3;
+        private static string Answer4;
+
         public static void Main(string[] args)
         {
-            //Variable declaration
-            string value;
+            Console.WriteLine(Question1);
+            Answer1 = Console.ReadLine();
+            Console.WriteLine(Question2);
+            Answer2 = Console.ReadLine();
+            Console.WriteLine(Question3);
+            Answer3 = Console.ReadLine();
+            Console.WriteLine(Question4);
+            Answer4 = Console.ReadLine();
+            int age = int.Parse(Answer2);
+            int day = int.Parse(Answer3);
+            int budget = int.Parse(Answer4);
 
-            //Assign Variable
-            value = " Have a nice day!";
 
-            //Print Variable
-            Test.HelloName("Elena" + value);
 
-            //Assign new value for Variable
-            value = "Starting Calculation";
-
-            //Print new value
-            Console.WriteLine(value);
-
-            //Calculate a sum from our new method
-            Console.WriteLine(Sum(2, 3));
-        }
-
-        public static int Sum (int value1, int value2)
+            if (age > 0 && age < 99)
             {
-            return value1 + value2;
-             }
+                if (age < 16)
+                {
+                    Console.WriteLine(Greeting + Answer1 + "! Sorry you are too small!");
+                }
+                else if (age == 17)
+                {
+                    Console.WriteLine(Greeting + Answer1 + "! You almost reached the age of majority!");
+                }
+                else
+                {
+                    Console.WriteLine(Greeting + Answer1 + "! You are old enough to explore our content!");
+                }
+            }
+
+            switch (day)
+            {
+                case 1:
+                    Console.WriteLine("Monday and we are working! Enjoy!");
+                    break;
+                case 2:
+                    Console.WriteLine("Tuesday and we are working! Enjoy!");
+                    break;
+                case 3:
+                    Console.WriteLine("Wednesday and we are working! Enjoy!");
+                    break;
+                case 4:
+                    Console.WriteLine("Thursday and we are working! Enjoy!");
+                    break;
+                case 5:
+                    Console.WriteLine("Friday and we are working! Enjoy!");
+                    break;
+                case 6:
+                    Console.WriteLine("Saturday, sorry this is a day off and we are not working!");
+                    break;
+                case 7:
+                    Console.WriteLine("Sunday, sorry this is a day off and we are not working!");
+                    break;
+
+            }
+
+            switch (budget)
+            {
+                case int n when n >= 0 && n <= 25:
+                    Console.WriteLine("Silver subscription may be available for you!");
+                    break;
+                case int n when n >= 26 && n <= 50:
+                    Console.WriteLine("Gold subscription may be available for you!");
+                    break;
+                case int n when n > 50:
+                    Console.WriteLine("Platinum subscription may be available for you!");
+                    break;
+                default:
+                    Console.WriteLine("We do not provide services on credit!");
+                    break;
+            }
+
+        }
     }
 }
